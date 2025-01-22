0f2b9984426640664f8e36cdd951f7a333ad22f3
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2025-01-22 17:58:41 +0700

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route} from "react-router";
+
+import Home from './pages/Home'
+import About from './pages/About'
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+import { Button } from 'react-bootstrap';
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Button>Click me!</Button>
+        </div>
+    );
+}
+
+export default Home;
