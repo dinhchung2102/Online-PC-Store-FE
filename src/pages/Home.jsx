b31634ef150d0bb459bc72de45fcaf6bf2f1cacc
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2025-01-22 18:34:19 +0700

    Add Font Awesome icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+
 
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <Container>
+            <h1>Hello my friend <FontAwesomeIcon icon={faHeart} /></h1>
+            
             <Button>Click me!</Button>
-        </div>
+        </Container>
     );
 }
 
