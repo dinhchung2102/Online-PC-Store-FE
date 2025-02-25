0c9f09a9b650d22c84af8776e4ca0b584440744f
unknown <luuly1163@gmail.com>
2025-02-25 12:07:08 +0700

    create shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router";
 import Home from './pages/Home'
 import About from "./pages/About"
 import Configuration_Design from "./pages/Configuration_Design";
-
+import Shopping_Cart from "./pages/Shopping_Cart";
 function App() {
 
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/configuration-design" element={<Configuration_Design />} />
+        <Route path="/shopping-cart" element={<Shopping_Cart />} />
       </Routes>
     </Router>
   )
