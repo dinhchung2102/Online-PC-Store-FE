843e929f390bd3139fde90e5cf5b99e83d4f7e3a
unknown <luuly1163@gmail.com>
2025-02-25 09:30:39 +0700

    update component product list modal + product card

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Card, CardContent, CardMedia, Typography, Button, Box } from '@mui/material';
+import { Link } from "react-router";
+const ProductCard = ({ product }) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <Link href='#' target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+        <CardMedia
+          component="img"
+          height="233"
+          image={product.image}
+          alt={product.name}
+        />
+        <CardContent>
+          <Typography 
+            variant="body3" 
+            component="div" 
+            sx={{ 
+              fontWeight: 'bold', 
+              color: 'black',
+              whiteSpace: 'nowrap', 
+              overflow: 'hidden', 
+              textOverflow: 'ellipsis', 
+              maxWidth: '100%' 
+            }}
+          >
+            {product.name}
+          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', marginTop: 1}}>
+            <Typography variant="body2" color="text.secondary" sx={{ textDecoration: 'line-through' }}>
+              {product.originalPrice}
+            </Typography>
+            <Typography variant="body3" color="primary" fontWeight='bold'>
+              {product.discountedPrice}
+            </Typography>
+          </Box>
+        </CardContent>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', padding: '0 16px 16px' }}>
+          <Button variant="outlined" color="primary" href='#' target="_blank" sx={{textTransform: 'none'}}>
+            Xem chi tiết
+          </Button>
+          <Button variant="contained" color="error">
+            Chọn
+          </Button>
+        </Box>
+      </Link>
+    </Card>
+  );
+};
+
+export default ProductCard;
