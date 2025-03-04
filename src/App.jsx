14f4a5aa9520b58ce3a4ad494010ffb3f39215d0
unknown <luuly1163@gmail.com>
2025-03-04 17:27:24 +0700

    Article details

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Configuration_Design from "./pages/Configuration_Design";
 import Mouse_all from "./pages/Mouse_all";
 import Shopping_Cart from "./pages/Shopping_Cart";
 import ListProduct from "~/pages/ListProduct";
-
+import Article_Details from "./pages/Article_Details";
 import Detail_Account from "./pages/Detail_Account";
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
         <Route path="/configuration-design" element={<Configuration_Design />} />
         <Route path="/mouse-all" element={<Mouse_all />} />
         <Route path="/shopping-cart" element={<Shopping_Cart />} />
-
+        <Route path="/article-details" element={<Article_Details />} />
         <Route path="/Detail_Account" element={<Detail_Account />} />
         <Route path="/products/:category" element={<ListProduct />} />
 
