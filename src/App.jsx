4c27042b3a6f1eff43c55a03b2d463327f9a5751
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2025-03-13 19:39:03 +0700

    List product #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Configuration_Design from "./pages/Configuration_Design";
 import Mouse_all from "./pages/Mouse_all";
 import Shopping_Cart from "./pages/Shopping_Cart";
@@ -13,7 +12,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route
           path="/configuration-design"
           element={<Configuration_Design />}
