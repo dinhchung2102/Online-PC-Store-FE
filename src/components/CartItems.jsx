fa2504a839edd117ad1525415005913351203c66
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2025-04-10 22:50:53 +0700

    update theme import in theme.js

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -13,73 +13,73 @@ const CartItem = ({ imgProduct }) => {
     setQuantity((prev) => prev + 1);
   };
   return (
-            <Grid
-              container
-              spacing={3}
-              sx={{  paddingBottom: 0 }}
-            >
-              <Grid item xs={2}>
-                <Box
-                  sx={{
-                    width: "8vh",
-                    height: "8vh",
-                    overflow: "hidden",
-                  }}
-                >
-                  <img
-                    src={imgProduct}
-                    alt="Product"
-                    style={{
-                      height: "100%",
-                      width: "auto",
-                    }}
-                  />
-                </Box>
-              </Grid>
-              <Grid
-                item
-                xs={7}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "flex-start",
-                  justifyContent: "space-around",
-                  gap: 1,
-                }}
-              >
-                <Typography sx={{ fontWeight: "bold" }}>
-                  Laptop ASUS Expertbook P1403CVA-i516-50W
-                </Typography>
-                <Typography sx={{ color: "#DF062D" }}>
-                  12.999.000đ
-                </Typography>
-              </Grid>
-              <Grid
-                item
-                xs={3}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "flex-end",
-                  justifyContent: "space-around",
-                  gap: 1,
-                }}
-              >
-                <Button
-                  variant="outlined"
-                  size="small"
-                  color="error"
-                  startIcon={<DeleteIcon />}
-                >
-                  Xóa
-                </Button>
-                <ButtonGroup size="small" aria-label="Small button group">
-                <Button color="secondary" onClick={handleDecrease}>-</Button>
-        <Button color="secondary" >{quantity}</Button>
-        <Button color="secondary" onClick={handleIncrease}>+</Button>
-                </ButtonGroup>
-              </Grid>
-            </Grid>
+    <Grid
+      container
+      spacing={3}
+      sx={{ paddingBottom: 0 }}
+    >
+      <Grid size={{ xs: 2 }} >
+        <Box
+          sx={{
+            width: "8vh",
+            height: "8vh",
+            overflow: "hidden",
+          }}
+        >
+          <img
+            src={imgProduct}
+            alt="Product"
+            style={{
+              height: "100%",
+              width: "auto",
+            }}
+          />
+        </Box>
+      </Grid>
+      <Grid
+
+        size={{ xs: 2 }}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "space-around",
+          gap: 1,
+        }}
+      >
+        <Typography sx={{ fontWeight: "bold" }}>
+          Laptop ASUS Expertbook P1403CVA-i516-50W
+        </Typography>
+        <Typography sx={{ color: "#DF062D" }}>
+          12.999.000đ
+        </Typography>
+      </Grid>
+      <Grid
+
+        size={{ xs: 2 }}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-end",
+          justifyContent: "space-around",
+          gap: 1,
+        }}
+      >
+        <Button
+          variant="outlined"
+          size="small"
+          color="error"
+          startIcon={<DeleteIcon />}
+        >
+          Xóa
+        </Button>
+        <ButtonGroup size="small" aria-label="Small button group">
+          <Button color="secondary" onClick={handleDecrease}>-</Button>
+          <Button color="secondary" >{quantity}</Button>
+          <Button color="secondary" onClick={handleIncrease}>+</Button>
+        </ButtonGroup>
+      </Grid>
+    </Grid>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,18 @@ import CssBaseline from '@mui/material/CssBaseline'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme.js'
-
+import { ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssVarsProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Provider store={store}>
         <App />
       </Provider>
-    </CssVarsProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
